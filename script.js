3e8887de9cd544dda5196738440324f22309bb19
Celia Saville <savi0027@algonquinlive.com>
2020-01-12 23:13:47 -0500

    java

---
+++ script.js
@@ -0,0 +1,17 @@
+
+const name = "Celia Saville"
+const number = "2"
+console.log("Hello, my name is Celia Saville!") 
+
+console.log(2)
+
+const number = number * 2
+
+const number = 4 * 5
+
+const number = 20 / 2
+
+const number = 10 - 7
+
+
+
